4fb6596eae809a8ef804be5e5c04aa8d2bd21a13
mdsalman0 <mdsalman01864346@gmail.com>
2022-06-27 04:50:12 +0600

    footer component the end

--- src/Pages/Sherad/Footer/Footer.js
+++ src/Pages/Sherad/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            <p style={{textAlign: 'center',background:' #999fad',padding:'30px',marginBottom:0,color:'white'}}> Copyright &copy; 2022-<script>document.write(new Date().getFullYear())</script> Your Name All Rights Reserved</p>
+            <p style={{textAlign: 'center',background:' #999fae',padding:'30px',marginBottom:0,color:'white'}}> Copyright &copy; 2022-<script>document.write(new Date().getFullYear())</script> Your Name All Rights Reserved</p>
         </div>
     );
 };
